dfbdaa9897789b5aec07cf6e48e4b76f4ddc8911
Yogesh Baisla <yogeshbaisla793@gmail.com>
2024-08-06 21:32:25 +0530

    Create Step2 FormComponentPage2

---
+++ src/Components/FormComponents/FormComponentPage2.js
@@ -0,0 +1,27 @@
+export function FormComponent(){
+    return(
+    <>
+    <form>
+        <div>
+        <label>Address Line 1 :</label>
+        <input type="text" placeholder="Address Line 1" required />
+        </div>
+        <div>
+        <label>Address Line 2 :</label>
+        <input type="text" placeholder="Address Line 2" required />
+        </div>
+        <div>
+        <label>City :</label>
+        <input type="text" placeholder="City" required />
+        </div>
+        <div>
+        <label>State :</label>
+        <input type="text" placeholder="State" required />
+        </div>
+        <div>
+        <label>Zip Code :</label>
+        <input type="number" placeholder="Zip Code" required />
+        </div>
+    </form>
+    </>);
+}
