b0ac03548d6a61763ff19b826e84e138a231c20a
Yogesh Baisla <yogeshbaisla793@gmail.com>
2024-08-07 22:48:49 +0530

    Setting and adjusting styling and app.js

--- src/App.js
+++ src/App.js
@@ -1,36 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
+import MultiStepForm from './Components/MultiStepForm';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import { FormComponentPage1 } from './Components/FormComponents/FormComponentPage1';
-import { FormComponentPage2 } from './Components/FormComponents/FormComponentPage2';
-import { NavigateLeft } from './Components/Navigation/Navigation';
-import { NavigateRight } from './Components/Navigation/NavigateRight';
 
 function App() {
-  const [currentVisible, setCurrentVisible] = useState("form1");
-
-  const handleNavigateLeft = () => {
-    console.log("clicked left")
-    if (currentVisible === "form2") {
-      setCurrentVisible("form1");
-    }
-  };
-
-  const handleNavigateRight = () => {
-    console.log("clicked Right")
-    if (currentVisible === "form1") {
-      setCurrentVisible("form2");
-    }
-  };
-
   return (
-    <div>
-      {console.log(currentVisible)}
-      {currentVisible === "form1" && <FormComponentPage1 id="form1" />}
-      {currentVisible === "form2" && <FormComponentPage2 id="form2" />}
-      <div>
-        <NavigateLeft onClick={handleNavigateLeft} />
-        <NavigateRight onClick={handleNavigateRight} />
-      </div>
+    <div className="App">
+      <MultiStepForm />
     </div>
   );
 }
